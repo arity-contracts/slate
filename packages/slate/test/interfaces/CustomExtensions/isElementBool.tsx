4e4a1b30e6ecf66e4c8f632de2e082c64b12e018
mdmjg <mdj308@nyu.edu>
2020-07-13 10:49:50 -0500

    custom-extensions

--- packages/slate/test/interfaces/CustomExtensions/isElementBool.tsx
+++ packages/slate/test/interfaces/CustomExtensions/isElementBool.tsx
@@ -1,7 +1,31 @@
-import { CustomExtensions } from "slate";
+import { CustomExtensions } from '../../../src/interfaces/custom-extensions';
+import { Element as BaseElement } from 'slate'
+import { ExtendedType } from 'slate';
 
-export const input = true;
-export const test = value => {
-  return CustomExtensions.isElement(value);
-};
-export const output = false;
+
+declare module 'slate' {
+  interface CustomExtensions {
+    Element:
+    | { type: "heading"; level: number }
+    | { type: "list-item"; depth: number }
+    Text: { bold?: boolean; italic?: boolean }
+  }
+}
+
+type Element = ExtendedType<"Element", BaseElement>
+
+const extension: CustomExtensions = {
+  Element: { type: "heading", level: 5 },
+  Text: { bold: true }
+}
+
+export const input = extension.Element as Element
+
+export const test = (element: Element) => {
+  if (element.type !== "heading") throw new Error(`Must be a heading`)
+  // Uncomment `element.depth` and you get a TypeScript error as desired
+  // element.depth
+  return element.level
+}
+
+export const output = 5;
